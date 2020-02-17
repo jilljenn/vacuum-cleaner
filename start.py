5617132c6bb81d0c341fef1d534b6803a0aac830
Jill-Jênn Vie <vie@jill-jenn.net>
2020-02-18 00:23:16 +0100

    Fix MountainCar

--- start.py
+++ start.py
@@ -1,71 +1,148 @@
-import gym
-env = gym.make("MountainCar-v0")
-#env = gym.make("CartPole-v1")
-state = env.reset()
-import numpy as np
-from autograd import grad
-import sys
+"""
+Episodic semi-gradient Sarsa for Gym's MountainCar v0
+JJ Vie, 2020
+"""
+# pylint: disable=redefined-outer-name,import-error,
+# pylint: disable=pointless-string-statement,no-member,unused-variable
 import random
+import gym
+import numpy as np
+import matplotlib.pyplot as plt  # For plotting actions
+from tiles3 import IHT, tiles
+# from autograd import grad
+
+env = gym.make("MountainCar-v0")
+# env = gym.make("CartPole-v1")
+actions = range(3)
+state = env.reset()
 
 GAMMA = 0.99
 ALPHA = 0.1
-LAMBDA = 1
-NB_STEPS = 5
+EPS = 0.9
+NB_EPISODES = 100
+TIMEOUT = 3000
 
-# (Just debug) Plot the rewards
-# import matplotlib.pyplot as plt
+iht = IHT(4096)
 
-def q(state, parameters):
-    return np.dot(parameters, state)
 
-# print(q(np.random.random(2), W))
-# sys.exit(0)
+def q(state, action, parameters):
+    """
+    Our function approximation for q(s, a)
+    """
+    return parameters[embed(state, action)].sum()
 
-def loss(parameters, new_state, reward, old_value):
-    return ALPHA * (reward + GAMMA * max(q(new_state, parameters)) - old_value)
+
+def embed(state, action):
+    """
+    States are embedded in {0, 1}^4096 using tile coding
+    See Richard S. Sutton's http://incompleteideas.net/tiles/tiles3.html
+    """
+    return tiles(iht, 8, [8 * (state[0] / (0.5 + 1.2)),
+                          8 * (state[1] / (0.07 + 0.07))], [action])
+
 
 def run_episode(env):
-    parameters = np.random.random((2, 2))
-    for _ in range(50):
-        print(_)
+    """
+    Run several episodes of sarsa
+    """
+    parameters = np.random.random(4096) * 2 - 1
+    all_states = []
+    length = {x: [] for x in range(NB_EPISODES)}
+    for n in range(NB_EPISODES):
+        if n:
+            print(n, np.mean(length[n - 1]))
         state = env.reset()
+        sarsa = [state]
+
+        # Choose A from S using eps-greedy policy derived from Q
+        action = (np.argmax([q(state, action, parameters)
+                             for action in actions]) if random.random() < EPS
+                  else random.choice(actions))
         # totalreward = 0
-        for _ in range(100):
-            # Choose A from S using policy derived from Q
-            action = np.argmax(q(state, parameters)) if random.random() < 0.9 else random.choice([0, 1])
-            # print(action)
-            
+        t = 0
+        while True:
+            if n == NB_EPISODES - 1:  # If final episode, let's show it
+                env.render()
+
             # Take action A, observe R, S'
-            new_state, reward, done, info = env.step(2 if action == 1 else action)
-            # print('all', q(state, parameters))
-            # print('that', q(state, parameters)[action])
+            new_state, reward, done, info = env.step(action)
+            all_states.append(new_state)
+
+            # If state S' is terminal
+            if done:
+                length[n].append(len(sarsa))
+                parameters[embed(state, action)] += ALPHA * (
+                    reward - q(state, action, parameters))
+                break  # Go to next episode
+
+            # Choose A' as a eps-greedy function of q(S', .)
+            new_action = (
+                np.argmax([q(new_state, action, parameters)
+                           for action in actions]) if random.random() < EPS
+                else random.choice(actions))
+
+            # If we want to remember everything
+            # sarsa.extend((action, reward, new_state))
+            sarsa.append(new_state)
+
+            parameters[embed(state, action)] += ALPHA * (
+                reward + GAMMA * q(new_state, new_action, parameters) -
+                q(state, action, parameters))
+
+            if t == TIMEOUT:
+                # print(sarsa)
+                length[n].append(len(sarsa))
+                break
+
+            state = new_state
+            action = new_action
+
+            t += 1
+
+            """
+            # That was q-learning (off-policy)
             old_value = q(state, parameters)[action]
-            new_value = old_value + ALPHA * (reward + GAMMA * max(q(new_state, parameters)) - old_value)
-        
-            # Gradient descent parameters
+            new_value = (old_value + ALPHA *
+                         (reward + GAMMA * max(q(new_state, parameters)) -
+                          old_value))
+
+            # Gradient descent
             for _ in range(NB_STEPS):
-                # print(action)
-                that = q(state, parameters)[action]
-                # print(that, new_value, new_value - that)
-                # print('p', parameters)
-                # print('nabla', grad(lambda parameters: loss(parameters, new_state, reward, old_value))(parameters))
-                parameters -= LAMBDA * grad(lambda parameters: loss(parameters, new_state, reward, old_value))(parameters)
-            # totalreward += reward
+                parameters -= LAMBDA * grad(loss)(parameters)
+            totalreward += reward
             # Until S is terminal
             if done:
-                break
+                break"""
     # return totalreward
-    return parameters
+    return parameters, all_states
 
-parameters = run_episode(env)
 
-# sys.exit(0)
+# Training
+parameters, all_states = run_episode(env)
+all_ = np.array(list(all_states))
+print(all_.min(axis=0), all_.max(axis=0))  # Some stats about the states
+
+# Plot the best action
+points = {action: [[], []] for action in actions}
+for x in np.linspace(-1.2, 0.5, 50):
+    for y in np.linspace(-0.08, 0.08, 50):
+        best_action = np.argmax([q([x, y], action, parameters)
+                                 for action in actions])
+        points[best_action][0].append(x)
+        points[best_action][1].append(y)
+
+plt.title('Best action')
+for action in points:
+    plt.scatter(points[action][0], points[action][1],
+                c='rgb'[action], label=['left', 'stop', 'right'][action])
+plt.legend()
+plt.show()
 
 """
 rewards = []
-bestparams = None  
+bestparams = None
 bestreward = -float('inf')
-for _ in range(500):  
+for _ in range(500):
     # parameters = np.random.rand(2) * 2 - 1
     reward = run_episode(env)
     rewards.append(reward)
@@ -74,25 +151,29 @@ for _ in range(500):
         bestparams = parameters
         # considered solved if the agent lasts 200 timesteps
         if reward == 200:
-            break
-"""
+            break"""
 
-# You do not need
-# plt.hist(rewards)
-# plt.show()
-        
-print(env.action_space)
+
+state = env.reset()
 for _ in range(500):
     env.render()
+    # Random
     # action = env.action_space.sample()  # pick a random action
-    # print(type(bestparams))
-    # print(type(observation))
-    # action = 0 if np.dot(bestparams,observation) < 0 else 2
-    # action = 2
-    action = np.argmax(q(state, parameters))
-    
-    # action = 1
-    print(action)
-    state, reward, done, info = env.step(2 if action == 1 else action)
+
+    # Dot product
+    # action = 0 if np.dot(bestparams, observation) < 0 else 2
+
+    # Greedy
+    # action = np.argmax(q(state, action, parameters) for action in range(3))
+
+    # Epsilon greedy
+    action = (np.argmax([q(state, action, parameters)
+                         for action in actions]) if random.random() < EPS
+              else random.choice(actions))
+
+    state, reward, done, info = env.step(action)
+    if done:
+        print('hooray', info)
+        break
 
 env.close()
